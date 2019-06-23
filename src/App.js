88de453225c674ad14aac37c87b5b9ad5786db88
wentiansky <2087152746@qq.com>
2019-06-24 07:50:49 +0800

    下棋游戏整体搭建

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 // import { Provider } from 'react-redux';
 // import store from './redux/store';
-import RouterDemo from './router-demo/index';
+// import RouterDemo from './router-demo/index';
+import Game from './pages/play-chess/index';
 
 import './App.css';
 
 function App() {
   return (
-    <RouterDemo />
+    <Game />
+    // <RouterDemo />
   );
 }
 
---
+++ src/pages/play-chess/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Square extends Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+    return (
+      <div>
+        <h4 className="status">{status}</h4>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>
+            {/* status */}
+          </div>
+          <ol>
+            {/* TODO */}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
